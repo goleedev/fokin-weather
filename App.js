cf41e9129b8241ce742340c8b9793eaffbca26d9
GO Lee <golee.dev@gmail.com>
2021-02-09 21:05:10 +0900

    0.4 Location

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import Loading from "./Loading";
+import * as Location from "expo-location";
 
 export default function App() {
-  return <Loading />;
+  export default class extends React.Component {
+    getLocation = async () => {
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+    };
+    componentDidMount() {
+      this.getLocation();
+    }
+    render() {
+      return <Loading />;
+    }
+  }
 }
 
 const styles = StyleSheet.create({
